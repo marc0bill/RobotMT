e90f9ca6b1d41378e60d4c4b4b93a4e8fb545a14
NathanRSL <nathan.roussel76@gmail.com>
2019-05-13 11:38:28 +0200

    Modification de l'algo de Reconnaissance

--- python/Reconnaissance/Reconnaissance.py
+++ python/Reconnaissance/Reconnaissance.py
@@ -13,7 +13,6 @@ def Reconnaissance():
 IM_WIDTH = 1280
 IM_HEIGHT = 720
 
-# This is needed since the working directory is the object_detection folder.
 sys.path.append('..')
 
 MODEL_NAME = 'ssdlite_mobilenet_v2_coco_2018_05_09' #Nom du dossier contenant le module de detection d'objet
@@ -29,16 +28,12 @@ PATH_TO_LABELS = os.path.join(CWD_PATH,'data','mscoco_label_map.pbtxt')
 # Nombre de classe que le détectteur d'objet peut voir
 NUM_CLASSES = 90
 
-## Load the label map.
-# Label maps map indices to category names, so that when the convolution
-# network predicts `5`, we know that this corresponds to `airplane`.
-# Here we use internal utility functions, but anything that returns a
-# dictionary mapping integers to appropriate string labels would be fine
+# On charge la modèle permettant la détection d'objet
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
-# Charge le Tensorflow model dans la memoire.
+# Charge le modèle Tensorflow dans la memoire.
 detection_graph = tf.Graph()
 with detection_graph.as_default():
     od_graph_def = tf.GraphDef()
@@ -50,21 +45,20 @@ with detection_graph.as_default():
     sess = tf.Session(graph=detection_graph)
 
 
-# Define input and output tensors (i.e. data) for the object detection classifier
+# Définie les tensors d'entrées et de sortie (i.e. données) pour la classification des objets
 
-# Input tensor is the image
+# Le tensor en entrée est une image (frame)
 image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
 
-# Output tensors are the detection boxes, scores, and classes
-# Each box represents a part of the image where a particular object was detected
+# Les tensors en sorties sont les boxs, les classes les scores associés 
+# Chaques box représente une partie de l'image où un objet a été détecté
 detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
 
-# Each score represents level of confidence for each of the objects.
-# The score is shown on the result image, together with the class label.
+# Chaque score représente le level de confience pour chaque objet détecté
 detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
 detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
 
-# Number of objects detected
+# Nombres d'objets détectés
 num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 
 
@@ -76,13 +70,12 @@ rawCapture.truncate(0)											#Remplie le tableau avec des zéros
 
 for frame1 in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):   #capture la caméra en continue 
 
-    # Acquire frame and expand frame dimensions to have shape: [1, None, None, 3]
-    # i.e. a single-column array, where each item in the column has the pixel RGB value
+	# Récupére une frame et change sa dimension de telle manière à avoir un tableau à une seule colonne, où chaque valeur représente un pixel
     frame = np.copy(frame1.array)  #copie le contenue du tableau frame1 dans frame
     frame.setflags(write=1)			#autorise l'écriture
     frame_expanded = np.expand_dims(frame, axis=0)
 
-    # Perform the actual detection by running the model with the image as input
+    # Réalise la détection avec en entrée l'image issue de la vidéo et stocke les résultats dans des tableaux
     (boxes, scores, classes, num) = sess.run([detection_boxes, detection_scores, detection_classes, num_detections],feed_dict={image_tensor: frame_expanded})
     global boxesr, classesr = boxes,classes
 
