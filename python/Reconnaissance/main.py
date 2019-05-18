fe7a7de7f12b6d0967be284729fb0638fbc12a5d
NathanRSL <nathan.roussel76@gmail.com>
2019-05-18 19:58:27 +0200

    Modif Reconnaissance + main et ajout du programme UART

--- python/Reconnaissance/main.py
+++ python/Reconnaissance/main.py
@@ -1,24 +1,8 @@
-import Reconnaissance
-import serial
-import sys
+import Reconnaissance as R
 
-	
-def main():
 
-	#Configuration du port série :
-	S = serial.Serial()			
-	S.baudrate = 9600
-	S.timeout = 0
-	S.port = "/dev/ttyAMA0"
 
-	try:
-		#On essaye d'ouvir le port série :
-		S.open()
-	except serial.SerialException as e:
-		# Le port serie ne s'ouvre pas
- 		sys.stderr.write("Impossible d'ouvrir le port serie %r: %s\n" % (s.port, e))
- 		sys.exit(1)
+#Test
+Liste,score=R.Detection()
 
- 	# On lance l'algorithme de reconnaissance d'image
- 	Reconnaissance()
- 	
+print(Liste,score)
