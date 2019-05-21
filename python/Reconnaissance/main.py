5f121a28b68f499cdf1153b93d2caff6d5bd7031
NathanRSL <nathan.roussel76@gmail.com>
2019-05-21 13:02:02 +0200

    Modif UART et main

--- python/Reconnaissance/main.py
+++ python/Reconnaissance/main.py
@@ -1,8 +1,25 @@
-import Reconnaissance as R
+import ReconnaissanceV2 as RV2
+import serial
+
+(category_index,image_tensor,detection_boxes,detection_scores,detection_classes,num_detections,sess)=RV2.load_model()
+
+(Liste,score)=RV2.Detection(category_index,image_tensor,detection_boxes,detection_scores,detection_classes,num_detections,sess)
 
 
 
-#Test
-Liste,score=R.Detection()
+#Configuration du port série :
+S = serial.Serial("/dev/ttyS0",9600)
 
-print(Liste,score)
+for x in Liste:
+	if x='laptop':
+		data='VtsM 50 50'
+		S.write(data.encode('ascii'))
+		receive=S.readline(len(data1))
+		print(receive.decode("utf-8"))
+		data='Stop'
+		S.write(data.encode('ascii'))
+		receive=S.readline(len(data))
+		print(receive.decode("utf-8"))
+
+
+S.close()
