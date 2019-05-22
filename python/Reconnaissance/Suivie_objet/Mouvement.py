0a9f68a0314093801498dc7be577300d547e196d
McDono <mcdonvan@outlook.fr>
2019-05-22 14:56:13 +0200

    fix problem indentation

--- python/Reconnaissance/Suivie_objet/Mouvement.py
+++ python/Reconnaissance/Suivie_objet/Mouvement.py
@@ -19,29 +19,28 @@ def reculer(vitesse, portSerie):
 	#UART.write(portSerie, commande)
 
 def tourner_sur_place(vitesse, direction, portSerie):
-    print("On tourne a ", direction)
-    if direction == "droite":
-        commande = "VtsM %f -%f" % (vitesse, vitesse)
-    elif direction == "gauche":
-        commande = "VtsM -%f %f" % (vitesse, vitesse)
-    else:
-        print("Erreur : direction incorrecte") #normalement on passe jamais par là
+	print("On tourne a ", direction)
+	if direction == "droite":
+		commande = "VtsM %f -%f" % (vitesse, vitesse)
+	elif direction == "gauche":
+		commande = "VtsM -%f %f" % (vitesse, vitesse)
+	else:
+		print("Erreur : direction incorrecte") #normalement on passe jamais par là
 
-    UART.write(portSerie, commande)
+	UART.write(portSerie, commande)
 	#On l'envoie plusieurs fois car nous avon quelques soucis avec le baudrate à ce stade
     #UART.write(portSerie, commande)
     #UART.write(portSerie, commande)
 
 
 def tourner_et_avancer(vitesse_alignement,vitesse_position, direction, portSerie):
-
-    if direction == "droite":
-        commande = "VtsM %f %f" % (vitesse_position + vitesse_alignement, vitesse_position - vitesse_alignement)    
-    elif direction == "gauche":
-        commande = "VtsM %f %f" % (vitesse_position - vitesse_alignement, vitesse_position + vitesse_alignement)  
-    else:
-        print("Erreur : direction incorrecte") #normalement on passe jamais par là
-    UART.write(portSerie, commande)
+	if direction == "droite":
+		commande = "VtsM %f %f" % (vitesse_position + vitesse_alignement, vitesse_position - vitesse_alignement)    
+	elif direction == "gauche":
+		commande = "VtsM %f %f" % (vitesse_position - vitesse_alignement, vitesse_position + vitesse_alignement)  
+	else:
+		print("Erreur : direction incorrecte") #normalement on passe jamais par là
+	UART.write(portSerie, commande)
 	#On l'envoie plusieurs fois car nous avon quelques soucis avec le baudrate à ce stade
     #UART.write(portSerie, commande)
     #UART.write(portSerie, commande)
--- python/Reconnaissance/Suivie_objet/main_V2.py
+++ python/Reconnaissance/Suivie_objet/main_V2.py
@@ -29,7 +29,7 @@ for p in range(NOMBRE_DE_PASSE):
 
 	for j in range(len(Liste_Objets_Reconnues)): #on scan tous les objets reconnues
 		if Liste_Objets_Reconnues[j][0] == nomObjetASuivre or Liste_Objets_Reconnues[j][0] == nomObjetAFuir :
-            print(Liste_Objets_Reconnues[j][0])
+			print(Liste_Objets_Reconnues[j][0])
 			#On crée un objet à partir de la liste recupérée de tensorflow
 			Objet_Tracke = ObjetReconnu.ObjetReconnu(Liste_Objets_Reconnues[j][0], Liste_Objets_Reconnues[j][1], Liste_Objets_Reconnues[j][2], Liste_Objets_Reconnues[j][3], Liste_Objets_Reconnues[j][4], Liste_Objets_Reconnues[j][5])
 			if constantes.Mode == 0 :
@@ -40,7 +40,7 @@ for p in range(NOMBRE_DE_PASSE):
 					else : #si ce n'est pas l'objet à suivre, c'est l'objet à suivre au vu d'un if precedent
 						ordre = "fuir"
 					position = Asserv_V2.positioner_robot(Objet_Tracke, ordre, portSerie)
-           
+
 			elif constantes.Mode == 1:
 				Asserv_V2.aligner_positionner_robot(Objet_Tracke, nomObjetASuivre, portSerie)  
 	sleep(constantes.DELAIS_ACQUISITION)             
