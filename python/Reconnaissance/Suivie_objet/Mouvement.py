df3587e6f293977fba794fdc6f634c11908dc32d
McDono <mcdonvan@outlook.fr>
2019-05-22 14:42:57 +0200

    fix problem indentation

--- python/Reconnaissance/Suivie_objet/Mouvement.py
+++ python/Reconnaissance/Suivie_objet/Mouvement.py
@@ -15,11 +15,11 @@ def reculer(vitesse, portSerie):
 	commande = "VtsM -%f -%f" % (vitesse, vitesse)
 	UART.write(portSerie, commande)
 	#On l'envoie plusieurs fois car nous avon quelques soucis avec le baudrate à ce stade
-	UART.write(portSerie, commande)
-	UART.write(portSerie, commande)
+	#UART.write(portSerie, commande)
+	#UART.write(portSerie, commande)
 
 def tourner_sur_place(vitesse, direction, portSerie):
-	print("On tourne a ", direction)
+    print("On tourne a ", direction)
     if direction == "droite":
         commande = "VtsM %f -%f" % (vitesse, vitesse)
     elif direction == "gauche":
@@ -29,8 +29,8 @@ def tourner_sur_place(vitesse, direction, portSerie):
 
     UART.write(portSerie, commande)
 	#On l'envoie plusieurs fois car nous avon quelques soucis avec le baudrate à ce stade
-    UART.write(portSerie, commande)
-    UART.write(portSerie, commande)
+    #UART.write(portSerie, commande)
+    #UART.write(portSerie, commande)
 
 
 def tourner_et_avancer(vitesse_alignement,vitesse_position, direction, portSerie):
@@ -43,5 +43,5 @@ def tourner_et_avancer(vitesse_alignement,vitesse_position, direction, portSerie
         print("Erreur : direction incorrecte") #normalement on passe jamais par là
     UART.write(portSerie, commande)
 	#On l'envoie plusieurs fois car nous avon quelques soucis avec le baudrate à ce stade
-    UART.write(portSerie, commande)
-    UART.write(portSerie, commande)
+    #UART.write(portSerie, commande)
+    #UART.write(portSerie, commande)
