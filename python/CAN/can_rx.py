9b64001721b9d2ac30b0e0c71739e3e4762de5d7
Abdel <massouri.abdelfatteh@gmail.com>
2019-05-26 02:18:37 +0200

    Create can_rx.py

---
+++ python/CAN/can_rx.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+#
+# can_rx.py
+# 
+# This is simple CAN receive python program. All messages received are printed out on screen.
+# For use with CAN SPI CLICK boards on the Raspberry Pi
+#
+# Make sure Python-CAN is installed first
+#
+# 26-05-19 MASSOURI Abdelfattah
+#
+#
+#
+
+import can
+import time
+import os
+
+
+print('\n\rCAN Rx test')
+print('Bring up CAN0....')
+os.system("sudo /sbin/ip link set can0 up type can bitrate 500000")
+time.sleep(0.1)	
+
+try:
+	bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
+except OSError:
+	print('Cannot find PiCAN board.')
+	exit()
+	
+print('Ready')
+
+try:
+	while True:
+		message = bus.recv()	# Wait until a message is received.
+		
+		c = '{0:f} {1:x} {2:x} '.format(message.timestamp, message.arbitration_id, message.dlc)
+		s=''
+		for i in range(message.dlc ):
+			s +=  '{0:x} '.format(message.data[i])
+			
+		print(' {}'.format(c+s))
+	
+	
+except KeyboardInterrupt:
+	#Catch keyboard interrupt
+	os.system("sudo /sbin/ip link set can0 down")
+	print('\n\rKeyboard interrtupt')
