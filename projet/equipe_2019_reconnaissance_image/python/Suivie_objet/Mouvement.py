26fd3d58ba98e8638d9c8bbbccb665cdae179f95
McDono <mcdonvan@outlook.fr>
2019-05-27 09:34:08 +0200

    correction code python

--- projet/equipe_2019_reconnaissance_image/python/Suivie_objet/Mouvement.py
+++ projet/equipe_2019_reconnaissance_image/python/Suivie_objet/Mouvement.py
@@ -4,7 +4,7 @@ import UART
 
 def avancer(vitesse, portSerie):
 	print("En marche avant")
-	commande = "VtsM %f %f" % (vitesse, vitesse)
+	commande = "VtsM %.0f %.0f" %(vitesse, vitesse)
 	UART.write(portSerie, commande)
 	#On l'envoie plusieurs fois car nous avon quelques soucis avec le baudrate à ce stade
 	UART.write(portSerie, commande)
@@ -12,7 +12,7 @@ def avancer(vitesse, portSerie):
 
 def reculer(vitesse, portSerie):
 	print("En marche arrière")
-	commande = "VtsM -%f -%f" % (vitesse, vitesse)
+	commande = "VtsM -%.0f -%.0f" %(vitesse, vitesse)
 	UART.write(portSerie, commande)
 	#On l'envoie plusieurs fois car nous avon quelques soucis avec le baudrate à ce stade
 	UART.write(portSerie, commande)
@@ -21,9 +21,9 @@ def reculer(vitesse, portSerie):
 def tourner_sur_place(vitesse, direction, portSerie):
 	print("On tourne a ", direction)
 	if direction == "droite":
-		commande = "VtsM %f -%f" % (vitesse, vitesse)
+		commande = "VtsM %.0f -%.0f" %(vitesse, vitesse)
 	elif direction == "gauche":
-		commande = "VtsM -%f %f" % (vitesse, vitesse)
+		commande = "VtsM -%.0f %.0f" %(vitesse, vitesse)
 	else:
 		print("Erreur : direction incorrecte") #normalement on passe jamais par là
 
@@ -35,9 +35,9 @@ def tourner_sur_place(vitesse, direction, portSerie):
 
 def tourner_et_avancer(vitesse_alignement,vitesse_position, direction, portSerie):
 	if direction == "droite":
-		commande = "VtsM %f %f" % (vitesse_position + vitesse_alignement, vitesse_position - vitesse_alignement)    
+		commande = "VtsM %.0f %.0f" %(vitesse_position + vitesse_alignement, vitesse_position - vitesse_alignement)    
 	elif direction == "gauche":
-		commande = "VtsM %f %f" % (vitesse_position - vitesse_alignement, vitesse_position + vitesse_alignement)  
+		commande = "VtsM %.0f %.0f" %(vitesse_position - vitesse_alignement, vitesse_position + vitesse_alignement)  
 	else:
 		print("Erreur : direction incorrecte") #normalement on passe jamais par là
 	UART.write(portSerie, commande)
@@ -50,9 +50,9 @@ def tourner_et_avancer(vitesse_alignement,vitesse_position, direction, portSerie
 	
 def tourner_et_reculer(vitesse_alignement,vitesse_position, direction, portSerie):
 	if direction == "droite":
-		commande = "VtsM %f %f" % (-vitesse_position + vitesse_alignement, -vitesse_position - vitesse_alignement)    
+		commande = "VtsM %f %f" %(-vitesse_position + vitesse_alignement, -vitesse_position - vitesse_alignement)    
 	elif direction == "gauche":
-		commande = "VtsM %f %f" % (-vitesse_position - vitesse_alignement, -vitesse_position + vitesse_alignement)  
+		commande = "VtsM %f %f" %(-vitesse_position - vitesse_alignement, -vitesse_position + vitesse_alignement)  
 	else:
 		print("Erreur : direction incorrecte") #normalement on passe jamais par là
 	UART.write(portSerie, commande)
