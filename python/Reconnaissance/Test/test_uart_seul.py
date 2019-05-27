6b26bb8395e70b2f2806bac1c7c36ae197620ff9
McDono <mcdonovan@outlook.fr>
2019-05-27 08:55:39 +0200

    modif sur la rasp

--- python/Reconnaissance/Test/test_uart_seul.py
+++ python/Reconnaissance/Test/test_uart_seul.py
@@ -7,18 +7,19 @@ vitesse = 200
 
 #Configuration du port série :
 print("Configuration du port serie...")
-S = serial.Serial("/dev/ttyS0",baudrate)
+S = serial.Serial("/dev/ttyS0",BAUDRATE)
 
 
-for j in range (len(L)):
-	for k in range NOMBRE_DE_PASSE:
-		data="VtsM %f %f" (vitesse, vitesse)
-		print("Envoie de la commande : ", data)
-		S.write(data.encode('ascii'))
-		#receive=S.readline(len(data))
-		#print(receive.decode("utf-8"))
-		sleep(1)
-	data="VtsM 0 0"
+for i in range(5):
+	data="VtsM 200 200\n"
+	print("Envoie de la commande : ", data)
+	S.write(data.encode('ascii'))
+	#sleep(1)
+	#receive=S.readline(len(data))
+	#print("Reception : ", receive.decode("utf-8"))
+	sleep(1)
+data="VtsM 0 0"
+for j in range(2):
 	print("Envoie de la commande : ", data)
 	S.write(data.encode('ascii'))
 	#receive=S.readline(len(data))
